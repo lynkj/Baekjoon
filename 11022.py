61ecdb543466fed1c89d155f69d2a6a47e2290b2
lynkj <jkim3281@gmail.com>
2025-01-09 15:31:00 +0900

    Add files via upload

--- 11022.py
+++ 11022.py
@@ -1,8 +1,7 @@
 T = int(input())
+n = 1
 
 # 각 테스트 케이스 처리
 for i in range(1, T + 1):
-    # 두 정수 A와 B 입력
-    A, B = map(int, input().split())
-    # 결과 출력
-    print(f"Case #{i}: {A} + {B} = {A + B}")
+    print("*" * n)
+    n += 1
